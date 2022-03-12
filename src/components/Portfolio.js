6f1aebbc1f50480cfb7e81ac3e308c0158b513aa
Aaron Ray <aray3030@gmail.com>
2022-03-12 09:54:29 -0500

    Portfolio  Component Complete

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,7 +7,8 @@ import { faDesktop, faFileCode } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 //  React Popup Box //
-import {PopupboxManager} from 'reactjs-popup';
+import {Popup} from 'reactjs-popup';
+import "reactjs-popup/dist/";
 
 
 
@@ -18,15 +19,32 @@ const Portfolio = () => {
 
     // Nucamp 
     const popupboxManagerNucamp = () => {
-        <>
-        <img src={Nucamp} alt= " NuCamp project..."/>
-        <p>Sportacus andrew weatherall goose Refined gentlemen super mario des lynam alpha trion zap rowsdower.
-                omar sharif old man in pub burt reynolds alpha trion sportacus villain Refined gentlemen zap rowsdower funny walk super mario frightfully nice.
-                andrew weatherall des lynam goose. Semi beard cream bun disaster alpha trion?
-        </p>
-        <b>Github link</b> <a className="hyper-link">Insert your git hub here </a>
-        </>
+        const content =(
+            <>
+            <img className="portfolio-image-popupbox" src={Nucamp} alt= " NuCamp project..."/>
+            <p>Sportacus andrew weatherall goose Refined gentlemen super mario des lynam alpha trion zap rowsdower.
+                    omar sharif old man in pub burt reynolds alpha trion sportacus villain Refined gentlemen zap rowsdower funny walk super mario frightfully nice.
+                    andrew weatherall des lynam goose. Semi beard cream bun disaster alpha trion?
+            </p>
+            <b>Github link</b> <a className="hyper-link" onClick={() =>  window.open("https://github.com/Aray30/React-Portfolio")}>https://github.com/Aray30/React-Portfolio </a>
+            </>
+         )
+
+
+        Popup.open({content})
     }
+
+    const popupboxConfigNucamp = {
+        titleBar: {
+            enable: true,
+            Text: "NuCamp Project"
+        },
+            fadeIn: true,
+            fadeInSpeed:500 
+    }
+
+        
+
 
 
   return (
@@ -34,7 +52,7 @@ const Portfolio = () => {
         <div className="container">
             <h1 className="text-uppercase text-center py-5">Portfolio</h1>
             <div className="image-box-wrapper row justify-content-center">
-                <div className='portfolio-image-box'>
+                <div className='portfolio-image-box' onClick={popupboxManagerNucamp }>
                     <img className="portfolio-image" src={Nucamp} alt="NuCamp RV"/>
                     <div className='overflow'></div>
                     <FontAwesomeIcon className='portfolio-icon' icon={faSearchPlus} />
@@ -57,6 +75,7 @@ const Portfolio = () => {
                 </div>
             </div>
         </div>
+        <popupboxContianer {...popupboxConfigNucamp} />
     </div>
   )
 }
